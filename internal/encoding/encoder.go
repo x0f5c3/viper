a133b6a64507465cf61fe140981f153cb9f9de71
x0f5c3 <x0f5c3@tutanota.com>
2023-06-20 16:45:55 +0200

    feat(Generics): First changes to generics

--- internal/encoding/encoder.go
+++ internal/encoding/encoder.go
@@ -1,13 +1,78 @@
 package encoding
 
 import (
+
 	"sync"
+
 )
+
+type GenericKV[V any] struct {
+	key string
+	val V
+}
+
+func (g *GenericKV[V]) Key() string {
+	return g.key
+}
+
+func (g *GenericKV[V]) SetKey(key string) {
+	g.key = key
+}
+
+func (g *GenericKV[V]) Val() V {
+	return g.val
+}
+
+func (g *GenericKV[V]) SetVal(val V) {
+	g.val = val
+}
+
+type KVMerge struct {
+	m map[string]GenericKV[any]
+}
+
+type GenericEncoder interface {
+	Encode(v map[string]GenericKV[any]) ([]byte, error)
+}
 
 // Encoder encodes the contents of v into a byte representation.
 // It's primarily used for encoding a map[string]interface{} into a file format.
 type Encoder interface {
 	Encode(v map[string]interface{}) ([]byte, error)
+}
+
+// NewGenericEncoderRegistry returns a new, initialized EncoderRegistry.
+func NewGenericEncoderRegistry() *EncoderRegistry {
+	return &EncoderRegistry{
+		encoders: make(map[string]Encoder),
+	}
+}
+
+// RegisterEncoder registers an Encoder for a format.
+// Registering a Encoder for an already existing format is not supported.
+func (e *EncoderRegistry) RegisterEncoder(format string, enc Encoder) error {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
+	if _, ok := e.encoders[format]; ok {
+		return ErrEncoderFormatAlreadyRegistered
+	}
+
+	e.encoders[format] = enc
+
+	return nil
+}
+
+func (e *EncoderRegistry) Encode(format string, v map[string]interface{}) ([]byte, error) {
+	e.mu.RLock()
+	encoder, ok := e.encoders[format]
+	e.mu.RUnlock()
+
+	if !ok {
+		return nil, ErrEncoderNotFound
+	}
+
+	return encoder.Encode(v)
 }
 
 const (
@@ -17,6 +82,11 @@ const (
 	// ErrEncoderFormatAlreadyRegistered is returned when an encoder is already registered for a format.
 	ErrEncoderFormatAlreadyRegistered = encodingError("encoder already registered for this format")
 )
+
+type GenericEncoderRegistry struct {
+	encoders map[string]GenericEncoder
+	mu       sync.RWMutex
+}
 
 // EncoderRegistry can choose an appropriate Encoder based on the provided format.
 type EncoderRegistry struct {
